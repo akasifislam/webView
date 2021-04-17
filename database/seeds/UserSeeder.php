dd670738cecd1b5fd24dd025012c3818c84be007
akasifislam <aseasifislam@gmail.com>
2021-04-17 22:23:13 +0600

    webviewer application

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'asif ul islam',
+            'email' => 'admin@mail.com',
+            'password' => Hash::make('secret007'),
+        ]);
+    }
+}
